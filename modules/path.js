9dbb17850e01260688ff4018166dcd7bf6de4fa6
Lucas Gomes <lukasmck@outlook.com>
2025-04-09 08:21:10 -0300

    atualizando os arquivos para o repositorio no git

--- modules/path.js
+++ modules/path.js
@@ -1,4 +1,16 @@
-const path = require('path')
+const path = require ('path')
 
-// basename
+// Apenas o nome do arquivo atual
 console.log(path.basename(__filename))
+
+// Nome do diretorio atual
+console.log(path.dirname(__filename))
+
+// Extensão do arquivo
+console.log(path.extname(__filename))
+
+// Criar objeto Path
+console.log(path.parse(__filename))
+
+// Juntar caminhos de arquivos
+console.log(path.join(__dirname, 'teste', 'teste.html'))
--- person.js
+++ person.js
@@ -4,8 +4,16 @@ class Person {
     }
 
     sayMyName() {
-        return `Hello! My name is ${this.name}!`
+        return `Hello there! \nMy name is ${this.name}!`
     }
+
 }
 
-module.exports = Person
+// module.exports = Person
+// para importar aqui, utilizar a sintaxe const Person = require('./person')
+
+// utilizar o proximo caso precise exportar mais de uma classe ou objeto
+module.exports = {
+    Person,
+}
+// para importar esse aqui, utilizar const { Person } = require('./person')
